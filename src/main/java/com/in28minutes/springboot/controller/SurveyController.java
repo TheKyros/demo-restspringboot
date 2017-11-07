8cb89d10612cc7bb91930a649c5e68e28f229746
Cyril BLAIN <Cyril.BLAIN@cgi.com>
2017-11-07 17:16:38 +0100

    Add custom response for add method

--- src/main/java/com/in28minutes/springboot/controller/SurveyController.java
+++ src/main/java/com/in28minutes/springboot/controller/SurveyController.java
@@ -1,14 +1,16 @@
 package com.in28minutes.springboot.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.in28minutes.springboot.service.SurveyService;
 import com.in28minutes.springboot.model.*;;
@@ -27,8 +29,19 @@ public class SurveyController {
 	
 	//GET "/surveys/{surveyId}/questions"
 	@PostMapping("/surveys/{surveyId}/questions")
-	public List<Question> addQuestionsToSurvey(@PathVariable String surveyId,@RequestBody Question newQuestion) {
-		return surveyService.retrieveQuestions(surveyId);
+	public ResponseEntity<?> addQuestionsToSurvey(@PathVariable String surveyId,@RequestBody Question newQuestion) {
+		Question question = surveyService.addQuestion(surveyId, newQuestion);
+		
+		if(question == null)
+		{
+			//Permet de renvoyer une réponse personnalisée
+			return ResponseEntity.noContent().build();
+		}
+		
+		//URI -> /surveys/{surveyId}/questions/{questionId} question.getQuestionId()
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(question.getId()).toUri();
+		
+        return ResponseEntity.created(location).build();
 	}
 	
 	/*
