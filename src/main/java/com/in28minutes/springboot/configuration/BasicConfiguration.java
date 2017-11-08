46c82fe43a9f060e3bf5eb6c49ba69ea9d7e968c
Cyril BLAIN <Cyril.BLAIN@cgi.com>
2017-11-08 13:31:49 +0100

    Add class BasicConfiguration

---
+++ src/main/java/com/in28minutes/springboot/configuration/BasicConfiguration.java
@@ -0,0 +1,38 @@
+package com.in28minutes.springboot.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("basic")
+public class BasicConfiguration {
+	private boolean value;
+	private String message;
+	private int number;
+	
+	public boolean isValue() {
+		return value;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public void setValue(boolean value) {
+		this.value = value;
+	}
+}
